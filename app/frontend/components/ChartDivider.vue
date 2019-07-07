354d181bcaef7132684cbcf239dc01376e013b82
asmirnov <asmirnov@solit-clouds.ru>
2019-07-07 03:49:20 +0300

    add legend

--- app/frontend/components/ChartDivider.vue
+++ app/frontend/components/ChartDivider.vue
@@ -1,10 +1,16 @@
 <template>
   <div class="developer-divider">
+    <label>
+      Рейтинг застройщиков
+    </label>
     <v-layout align-center>
       <v-spacer class="line"></v-spacer>
       <label class="region-caption">{{region ? region.text : ''}}</label>
       <v-spacer class="line"></v-spacer>
     </v-layout>
+    <label>
+      Минимальная стоимость м2
+    </label>
   </div>
 </template>
 
